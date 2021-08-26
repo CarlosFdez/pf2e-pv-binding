9fdb70dda00268aed04cbf66661c7dabe063c9e3
Carlos Fernandez <cfern1990@gmail.com>
2021-08-25 23:15:35 -0400

    release: 0.1.2 explicit token update to avoid PV multi-token bug

--- index.js
+++ index.js
@@ -1,38 +1,64 @@
 Hooks.on("init", () => {
-    const TokenClass = TokenDocument;
-    const defaultTokenPrepareBaseData = TokenClass.prototype.prepareBaseData;
-    TokenClass.prototype.prepareBaseData = function(...args) {
-        defaultTokenPrepareBaseData.apply(this, args);
-        const actor = this.actor;
-        if (["character", "npc", "familiar"].includes(actor?.type)) {
-            const newBrightSight = actor.hasDarkvision ? canvas.dimensions.maxR : 0;
-            const newDimSight = actor.hasLowLightVision ? canvas.dimensions.maxR : 0;
-            const isBrightestVision = actor.itemTypes.ancestry[0]?.slug === "fetchling";
-            if (isBrightestVision) {
-                this.data.update({
-                    "flags.perfect-vision": {
-                        visionRules: "custom",
-                        dimVisionInDarkness: "bright",
-                        dimVisionInDimLight: "bright",
-                        brightVisionInDarkness: "bright",
-                        brightVisionInDimLight: "bright",
-                    }
-                });
-            } else {
-                this.data.update({
-                    "flags.perfect-vision": {
-                        visionRules: "default"
-                    }
-                });
-            }
-            
-            if (newBrightSight !== this.data.brightSight || newDimSight !== this.data.dimSight) {
-                this.data.brightSight = newBrightSight;
-                this.data.dimSight = newDimSight;
-                if (this.object?.isControlled) {
-                    this.object.updateSource();
-                }
+    if (typeof libWrapper === "undefined") return;
+    libWrapper.register("pf2e-pv-binding", "CONFIG.Actor.documentClass.prototype.prepareData", actorPrepareData);
+});
+
+Hooks.on("preCreateToken", (token) => {
+    const actor = token.actor;
+    if (actor && token) {
+        const data = getVisionData(actor);
+        const updates = getTokenUpdates(token, data);
+        if (updates) {
+            token.data.update(updates);
+        }
+    }
+})
+
+function actorPrepareData(wrapped, ...args) {
+    wrapped(...args);
+    if (this.initialized && ["character", "npc", "familiar"].includes(this.type)) {
+        const data = getVisionData(this);
+        const tokens = canvas.ready ? this.getActiveTokens() : [];
+        for (const token of tokens) {
+            const updates = getTokenUpdates(token, data);
+            if (updates) {
+                token.update(updates);
             }
         }
     }
-});
+};
+
+function getVisionData(actor) {
+    return {
+        brightSight: actor.hasDarkvision ? 1000 : 0,
+        dimSight: actor.hasLowLightVision ? 1000 : 0,
+        isBrightest: actor.items.find(i => i.type === "ancestry")?.slug === "fetchling",
+    };
+}
+
+function getTokenUpdates(token, { brightSight, dimSight, isBrightest }) {
+    const actor = token.actor;
+
+    const updates = {};
+    const isAlreadyBrightest = actor.data.flags['perfect-vision']?.brightVisionInDarkness === "bright";
+    if (isBrightest && !isAlreadyBrightest) {
+        updates["flags.perfect-vision"] = {
+            visionRules: "custom",
+            dimVisionInDarkness: "bright",
+            dimVisionInDimLight: "bright",
+            brightVisionInDarkness: "bright",
+            brightVisionInDimLight: "bright",
+        };
+    } else if (!isBrightest && isAlreadyBrightest) {
+        updates["flags.perfect-vision"] = { visionRules: "default" };
+    }
+
+    if (token.data.brightSight !== brightSight || token.data.dimSight !== dimSight) {
+        updates.brightSight = brightSight;
+        updates.dimSight = dimSight;
+    }
+
+    if (Object.keys(updates).length > 0) {
+        return updates;
+    }
+}
